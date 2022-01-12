ad877daf27d1359ab09aba3deaaabeabf6a7dbdf
a1saipavan <76589153+a1saipavan@users.noreply.github.com>
2022-01-12 16:49:07 +0530

    removing available zone

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,6 @@ provider "aws" {
     region = "us-east-1"
 }
 
-variable avail_zone {
-    default = "us-east-1a"
-}
-
 resource "aws_s3_bucket" "preparation" {
   bucket = "sai-bucket"
   acl    = "private"
