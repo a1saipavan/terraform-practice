e0658eefdd913a53fbcacddbf071b7d1a8c47873
a1saipavan <76589153+a1saipavan@users.noreply.github.com>
2022-01-14 10:31:36 +0530

    changing s3bucket to iam user

---
+++ tf-iam.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_iam_user" "tf.user" {
+  name = "tf.user"
+
+  tags = {
+    Name        = "tf-user"
+    Environment = "Dev"
+  }
+}
