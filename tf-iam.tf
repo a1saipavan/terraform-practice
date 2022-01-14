d0fe4c5f47e13101da304e316e2312802830f3d4
a1saipavan <76589153+a1saipavan@users.noreply.github.com>
2022-01-14 11:05:45 +0530

    modifing user name

--- tf-iam.tf
+++ tf-iam.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_iam_user" "tf.user" {
-  name = "tf.user"
+resource "aws_iam_user" "tf_user" {
+  name = "tf_user"
 
   tags = {
     name = "tf-user"
